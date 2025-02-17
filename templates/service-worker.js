d4eb5e228760d6490569f87965cf160385febbe9
patman <68055660+pat-chye@users.noreply.github.com>
2025-02-17 04:46:34 +0000

    1

---
+++ templates/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'minimal-pwa-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install event: cache the important files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch event: serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Return cached response if found, else fetch from network
+      return response || fetch(event.request);
+    })
+  );
+});
