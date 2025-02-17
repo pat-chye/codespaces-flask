af500b5e8b1deef983c22ae64054363899a91587
patman <68055660+pat-chye@users.noreply.github.com>
2025-02-17 04:57:33 +0000

    5

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+# 1) Serve the main page from /static/index.html
+@app.route('/')
+def home():
+    return app.send_static_file('index.html')
+
+# 2) Serve the service worker at the root
+@app.route('/service-worker.js')
+def service_worker():
+    response = app.send_static_file('service-worker.js')
+    response.headers['Content-Type'] = 'application/javascript'
+    return response
+
+if __name__ == '__main__':
+    # Running on localhost:5000 by default
+    app.run(debug=True, host='0.0.0.0', port=5000)
