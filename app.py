0a4602e5233848baa4221589d09d45c593bd2470
patman <68055660+pat-chye@users.noreply.github.com>
2025-02-17 05:00:05 +0000

    1

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='static')
 
-# 1) Serve the main page from /static/index.html
 @app.route('/')
 def home():
     return app.send_static_file('index.html')
 
-# 2) Serve the service worker at the root
+# Optional: Serve service worker at root
 @app.route('/service-worker.js')
 def service_worker():
     response = app.send_static_file('service-worker.js')
     response.headers['Content-Type'] = 'application/javascript'
     return response
 
+# Optional: Serve manifest at root
+@app.route('/manifest.json')
+def manifest():
+    return app.send_static_file('manifest.json')
+
 if __name__ == '__main__':
-    # Running on localhost:5000 by default
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
